70993b536d0572a6f09bbec479ee828edaede5c0
Tarje Sælen Lavik <tarje.lavik@gmail.com>
2022-09-27 15:39:30 +0200

    add test 2

--- apps/nested-create-test/next.config.js
+++
@@ -1,9 +0,0 @@
-const withTM = require("next-transpile-modules")(["ui"]);
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
-
-module.exports = module.exports = withTM(nextConfig);
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ["next", "turbo", "next/core-web-vitals", "prettier"],
+  plugins: ["prettier"],
   settings: {
     react: {
       version: "detect",
