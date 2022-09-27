b157bfca2dac1f388e54661836350bcaf9430945
Tarje Sælen Lavik <tarje.lavik@gmail.com>
2022-09-27 09:17:06 +0200

    Remove to cards

--- apps/nested-create-test/pages/index.js
+++ apps/nested-create-test/pages/index.js
@@ -36,24 +36,6 @@ export default function Home() {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
             </div>
           </Link>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
